d05d308b95cca2e4733286475f2a9a1d11942f07
igor-rizak <igor.rizak@ukr.net>
2023-04-16 21:10:35 +0300

    hw 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,13 @@
 const totalCategories = document.querySelectorAll(".item");
-// console.log(totalCategories);
 console.log(`Number of categories: ${totalCategories.length}`);
 
-const categoriesArray = [...totalCategories]
-  .map(
-    categories => `Category: ${categories.children[0].textContent}
-    Elements: ${categories.children[1].children.length}`
-  )
-  .join("\n");
-  console.log(categoriesArray);
+
+
+totalCategories.forEach((element) => {
+
+  console.log(` Category:  ${ element.firstElementChild.innerText }`)
+  console.log(` Elements:  ${ element.children[1].children.length }`)
+
+})
+
+
